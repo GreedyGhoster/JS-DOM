e8f2a224660916e9fcab0fe847fa05291c901ab9
Ivan Nikitin <vananikitin95@gmail.com>
2022-10-08 23:20:02 +0300

    small upgrade (open block/close block)

--- lessons.js
+++ lessons.js
@@ -2,4 +2,9 @@ const button = document.querySelector("#button");
 const content = document.querySelector("#card");
 button.addEventListener("click", function () {
   content.classList.toggle("card-hidden");
+  if (content.classList.contains("card-hidden")) {
+    button.textContent = "Open block";
+  } else {
+    button.textContent = "Close block";
+  }
 });
