45c6e9144e4df61bd0e682789737f93cde071176
Ivan Nikitin <vananikitin95@gmail.com>
2022-10-09 20:48:38 +0300

    add accordeon (in lessons.js)

--- lessons.js
+++ lessons.js
@@ -1,10 +1,17 @@
+// open/close block
 const button = document.querySelector("#button");
 const content = document.querySelector("#card");
 button.addEventListener("click", function () {
-  content.classList.toggle("card-hidden");
-  if (content.classList.contains("card-hidden")) {
+  if (content.classList.toggle("card-hidden")) {
     button.textContent = "Open block";
   } else {
     button.textContent = "Close block";
   }
+});
+// accardeon
+const headers = document.querySelectorAll("[data-name='accordeon-title']");
+headers.forEach(function (items) {
+  items.addEventListener("click", function () {
+    this.nextElementSibling.classList.toggle("list-content");
+  });
 });
