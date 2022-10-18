9d96a347afaed5bb6833ac9a4c885517d1c3880a
Ivan Nikitin <vananikitin95@gmail.com>
2022-10-18 19:11:27 +0300

    Add modal windows

--- lessons.js
+++ lessons.js
@@ -28,23 +28,54 @@ tabHeaders.forEach(function (item) {
   });
 });
 // Modal window ------------
-const buttonOpenModal = document.querySelector("[data-modal-open]");
-const ModalContent = document.querySelector("[data-modal-window]");
-const buttonCloseModal = document.querySelector("[data-modal-close]");
-buttonOpenModal.addEventListener("click", function () {
-  ModalContent.classList.remove("hidden-parentblock");
+const buttonOpenModals = document.querySelectorAll("[data-modal-open]");
+const buttonCloseModals = document.querySelectorAll("[data-modal-close]");
+const allModals = document.querySelectorAll("[data-modal-window]");
+// Открытие модалки
+buttonOpenModals.forEach(function (opener) {
+  opener.addEventListener("click", function () {
+    const openModal = document.querySelector("#" + this.dataset.modalOpen);
+    openModal.classList.remove("hidden-parentblock");
+    // исключение при нажатии с целью закрыть окно
+    openModal
+      .querySelector(".modal-content")
+      .addEventListener("click", function (stoper) {
+        stoper.stopPropagation();
+      });
+  });
 });
-buttonCloseModal.addEventListener("click", function () {
-  ModalContent.classList.add("hidden-parentblock");
+// Закрытие по нажатию
+buttonCloseModals.forEach(function (closer) {
+  closer.addEventListener("click", function () {
+    const closeModal = this.closest("[data-modal-window]");
+    closeModal.classList.add("hidden-parentblock");
+  });
 });
-// Закрытие окна при нажатии вне контента
-ModalContent.addEventListener("click", function () {
-  ModalContent.classList.add("hidden-parentblock");
+// Закрытие по фейду
+allModals.forEach(function (fade) {
+  fade.addEventListener("click", function () {
+    this.classList.add("hidden-parentblock");
+  });
 });
-// исключение при нажатии с целью закрыть окно
-ModalContent.querySelector(".modal-content").addEventListener(
-  "click",
-  function (stoper) {
-    stoper.stopPropagation();
-  }
-);
+
+// Only 1 Modal window ------------
+// const buttonOpenModal = document.querySelector("[data-modal-open]");
+// const ModalContent = document.querySelector("[data-modal-window]");
+// const buttonCloseModal = document.querySelector("[data-modal-close]");
+// buttonOpenModal.addEventListener("click", function () {
+//   ModalContent.classList.remove("hidden-parentblock");
+// });
+// buttonCloseModal.addEventListener("click", function () {
+//   ModalContent.classList.add("hidden-parentblock");
+// });
+// // Закрытие окна при нажатии вне контента
+// ModalContent.addEventListener("click", function () {
+//   ModalContent.classList.add("hidden-parentblock");
+// });
+// // исключение при нажатии с целью закрыть окно
+// ModalContent.querySelector(".modal-content").addEventListener(
+//   "click",
+//   function (stoper) {
+//     stoper.stopPropagation();
+//   }
+// );
